7b8d4231318cfe533bac78d64f688e3927ae653b
yangjun zhu <yangjun.zhu@neulion.com.cn>
2016-10-21 18:33:27 +0800

    add animation to customContentView on demo

--- EZPlayerViewController/CustomPlayerViewController.m
+++ EZPlayerViewController/CustomPlayerViewController.m
@@ -50,5 +50,22 @@
 }
 
 
+- (void)playViewController:(EZPlayerViewController *)playViewController handleCustomContentView:(UIView *)customContentView isHidden:(BOOL)isHidden completionHandler:(void(^)())completionHandler{
+    self.customContentView.hidden = isHidden;
+    if (self.customContentView.hidden) {
+        [self.view sendSubviewToBack:self.customContentView];
+        completionHandler();
+    }else{
+        [self.view bringSubviewToFront:self.customContentView];
+        self.customContentView.alpha = 0;
+        [UIView animateWithDuration:1 animations:^{
+            self.customContentView.alpha = 1;
+        } completion:^(BOOL finished) {
+            completionHandler();
+        }];
+    }
+}
+
+
 
 @end
